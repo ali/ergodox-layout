88b53d21021e5e00aa4297f96bcda21bfa920d31
Ali Ukani <ali.ukani@gmail.com>
2017-10-28 19:40:23 -0400

    CLI w/ options for path to layout + screenshot filename

--- ergodox-keymap-screenshot/index.js
+++ ergodox-keymap-screenshot/index.js
@@ -1,8 +1,41 @@
-const puppeteer = require("puppeteer");
+const path = require("path");
 const fs = require("fs-extra");
+const puppeteer = require("puppeteer");
+const yargs = require("yargs");
+
+const pkg = require("./package.json");
+
+const argv = yargs.command(
+  "$0",
+  pkg.description,
+  yargs => {
+    return yargs.options({
+      f: {
+        alias: ["file", "layout"],
+        demandOption: true,
+        describe: "file path to a keyboard layout JSON file",
+        string: true
+      },
+      d: {
+        alias: ["dest", "screenshot"],
+        describe: "file path to save the screenshot to",
+        string: true,
+        default: "screenshot.png"
+      },
+      headless: {
+        describe: "run chromium in headless mode",
+        boolean: true,
+        default: true
+      }
+    });
+  },
+  argv => {
+    return getKeyboardLayoutScreenshot(argv.file, argv.dest, argv.headless);
+  }
+).argv;
 
 async function importLayout(page, layoutJson) {
-    console.log("Clicking import button")
+  console.log("Clicking import button");
   await page.waitFor("button#import-map");
   const importButton = await page.$("button#import-map");
   await importButton.click();
@@ -13,45 +46,53 @@ async function importLayout(page, layoutJson) {
 
   console.log("Injecting layout");
   const layoutTextArea = await page.$(".popup textarea");
-  await page.evaluate((textarea, layoutJson) => {
-    textarea.value = JSON.stringify(layoutJson, null, 2);
-    return Promise.resolve(textarea.value)
-  }, layoutTextArea, layoutJson);
+  await page.evaluate(
+    (textarea, layoutJson) => {
+      textarea.value = JSON.stringify(layoutJson, null, 2);
+      return Promise.resolve(textarea.value);
+    },
+    layoutTextArea,
+    layoutJson
+  );
 
-  console.log("Waiting")
+  console.log("Waiting");
   await page.waitFor(500);
 
-  console.log("Clicking submit")
+  console.log("Clicking submit");
   const submit = await page.$(".popup button.button-read");
   await submit.click();
 
-  console.log("Waiting for popup to disappear")
+  console.log("Waiting for popup to disappear");
   return await page.waitFor(500);
 }
 
-async function screenshotKeyboardLayout(page) {
-    console.log("Waiting for layout preview to appear");
+async function saveScreenshot(page, filename) {
+  console.log("Waiting for layout preview to appear");
   await Promise.all([page.waitFor("#stage"), page.waitFor("#stage .key")]);
   const container = await page.$("#container");
-    console.log("Say cheese");
-  return await container.screenshot({ path: "screenshot.png" });
+
+  console.log("Say cheese");
+  return await container.screenshot({ path: filename });
 }
 
-(async () => {
-  const layoutJson = await fs.readJson("../MDErgo1-Default.json");
+async function getKeyboardLayoutScreenshot(
+  pathToLayoutJson,
+  screenshotFilename,
+  headless
+) {
+  const layoutJson = await fs.readJson(path.resolve(pathToLayoutJson));
   const browser = await puppeteer.launch({
-    // headless: false,
+    headless,
     args: ["--disable-setuid-sandbox", "--no-sandbox"]
   });
+
   const page = await browser.newPage();
   await page.goto("https://configurator.inputclub.com/?layout=MDErgo1-Default");
 
-//   console.log(await page.content());
-
   await page.waitFor(".container");
   await importLayout(page, layoutJson);
-  await screenshotKeyboardLayout(page);
+  await saveScreenshot(page, screenshotFilename);
 
   console.log("Done");
   await browser.close();
-})();
+}
